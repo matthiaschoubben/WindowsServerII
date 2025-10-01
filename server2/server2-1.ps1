5556b6fc4d8852e20fc407561ef058901345f0b7
matthiaschoubben <matthias.schoubben@student.hogent.be>
2025-10-01 11:05:36 +0200

    server2-1 start

---
+++ server2/server2-1.ps1
@@ -0,0 +1,33 @@
+# IP instellen en Shared folder copyen
+
+# Variables
+$IFNAME = "Ethernet 2"
+$DNS = "192.168.25.11"
+$GW = "192.168.25.1"
+$IP = "192.168.25.21"
+$SF = "C:\vagrant"
+$LOCALPATH = "C:\Users\Public\shared_folder"
+
+# Static IP
+Write-Host "Configuring static IPv4"
+New-NetIPAddress -InterfaceAlias $IFNAME -IPAddress $IP -PrefixLength 24 -DefaultGateway $GW
+Set-DnsClientServerAddress -InterfaceAlias $IFNAME -ServerAddresses $DNS
+Write-Output "Static IP set to $IP on interface $IFNAME"
+
+# Copy shared folder locally
+Write-Output "Copying shared folder to the local path."
+if (!(Test-Path $LOCALPATH)) {
+    New-Item -Path $LOCALPATH -ItemType Directory -Force
+    Write-Output "Local path created at $LOCALPATH."
+}
+Copy-Item -Path $SF\* -Destination $LOCALPATH -Recurse -Force
+Write-Host "Shared folder successfully copied to $LOCALPATH."
+
+# Install required packages
+Write-Host "Installing required Windows features."
+Install-WindowsFeature -Name DNS -IncludeManagementTools
+Write-Output "All required packages installed successfully."
+
+# Reboot
+Write-Host "Rebooting system to apply changes."
+Restart-Computer -Force
